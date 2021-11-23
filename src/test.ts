ca94d6ffc64b00d4111b4c921478404d5b2bb9bf
Manoj Sethi <manojsethi@live.in>
2021-11-23 22:12:16 +0530

    M.Sethi => Issue Replication

--- src/test.ts
+++ src/test.ts
@@ -1,54 +1,12 @@
-// NodeJS: 16.11.1
-// MongoDB: 4.2-bionic (Docker)
-import { getModelForClass, prop, Ref, ReturnModelType } from '@typegoose/typegoose'; // @typegoose/typegoose@9.2.0
-import { AnyParamConstructor } from '@typegoose/typegoose/lib/types';
-import * as mongoose from 'mongoose'; // mongoose@6.0.12
-
-class Nested {
-  @prop()
-  public nested?: string;
-}
-
-const NestedModel = getModelForClass(Nested);
-
-class Base {
-  @prop()
-  public something?: string;
-
-  @prop({ ref: () => Nested })
-  public reference?: Ref<Nested>;
-}
-
-const BaseModel = getModelForClass(Base);
-
-function getModelForDb<T extends AnyParamConstructor<any>>(databaseName: string, model: ReturnModelType<T>): ReturnModelType<T> & T {
-  const db = mongoose.connection.useDb(databaseName);
-
-  const DbModel = db.model(model.modelName, model.schema) as ReturnModelType<T> & T;
-
-  return DbModel;
-}
+import * as mongoose from 'mongoose';
+import databaseConfig from './db/database.config';
+import BASE_CLIENTS_MODEL, { BaseClients } from './models/clients';
 
 (async () => {
-  await mongoose.connect(`mongodb://localhost:27017/`, {
-    dbName: 'verifyDefault',
-  });
-
-  const nestedInDefault = await NestedModel.create({ nested: 'helloInDefault1' });
-  const baseInDefault = await BaseModel.create({ something: 'HelloInDefault2', nested: nestedInDefault });
-
-  console.log('nestedInDefault', nestedInDefault);
-  console.log('baseInDefault', baseInDefault);
-
-  // purposefully in a different order
-  const cBaseModel = getModelForDb('verifyChanged', BaseModel);
-  const cNestedModel = getModelForDb('verifyChanged', NestedModel);
-
-  const nestedInChanged = await cNestedModel.create({ nested: 'helloInChanged1' });
-  const baseInChanged = await cBaseModel.create({ something: 'helloInChanged2', nested: nestedInChanged });
-
-  console.log('nestedInChanged', nestedInChanged);
-  console.log('baseInChanged', baseInChanged);
-
+  await mongoose.connect(`mongodb+srv://T9bd6gf47CWrgQKD:RWrqz2N4M44KbsFw@cluster0.3qybf.mongodb.net?retryWrites=true&w=majority`);
+  const client_id = databaseConfig.getModelForDb<typeof BaseClients>('base_db', BASE_CLIENTS_MODEL);
+  //base_db is a dynamic name, for testing purpose it is hardcoded.
+  const found_users = await client_id.findById(new mongoose.Types.ObjectId('619cee5091f3aaedfd5f8d80')).populate(['role']);
+  console.log(found_users);
   await mongoose.disconnect();
 })();
